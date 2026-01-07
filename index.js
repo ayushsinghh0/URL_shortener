f5e552e8362678a572324765a74ff8aa20ca1f97
ayushsinghh0 <ayushraj4820@gmail.com>
2026-01-08 05:10:03 +0530

    mongo db connection

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express=require("express");
 
+const {}=require ("./connect")
+
+const urlRoute = require('./routes/url');
+const { connectToMongoDB } = require("./connect");
+
 const app=express();
 const PORT=8001;
 
+connectToMongoDB('mongodb://localhost:')
 
-app.listen(PORT,()=> console.log('server started'))
+app.use("/url",urlRoute);
+
+app.listen(PORT,()=> console.log('server started' ))
